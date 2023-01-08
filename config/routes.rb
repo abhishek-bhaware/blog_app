f7e4b07759b34522cfa64804420f977af274d07b
abhishek.bhaware@synaptic.com <abhishek.bhaware@synaptic.com>
2023-01-09 02:39:39 +0530

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,15 @@
 Rails.application.routes.draw do
+  get 'demo/index'
   get 'welcome/index'
+  get 'demo/hello'
+  get 'demo/hello1'
+  get 'demo/youtube '
+  get 'pages#home'
+  get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root 'welcome#index'
+  # get ':controlller(/:action(/:id))'
 end
